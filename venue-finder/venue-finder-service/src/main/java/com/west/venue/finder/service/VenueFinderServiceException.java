0cb70f39bd0b3fd6d2cd4fa8ce94a9a5149132e8
Peter West <peterjameswest@sciencefictionextra.com>
2016-10-15 22:02:01 +0100

    Added exception for service layer to throw

---
+++ venue-finder/venue-finder-service/src/main/java/com/west/venue/finder/service/VenueFinderServiceException.java
@@ -0,0 +1,11 @@
+package com.west.venue.finder.service;
+
+/**
+ * Created by westp on 15/10/2016.
+ */
+public class VenueFinderServiceException extends Exception {
+
+    public VenueFinderServiceException(String message) {
+        super(message);
+    }
+}
