6d6404207a47b9874744780b5b17fab10a98c66c
Peter West <peterjameswest@sciencefictionextra.com>
2016-10-15 17:31:31 +0100

    Added skeleton service

---
+++ venue-finder/venue-finder-service/src/main/java/com/west/venue/finder/service/VenueFinderService.java
@@ -0,0 +1,13 @@
+package com.west.venue.finder.service;
+
+import java.util.List;
+
+/**
+ * Created by westp on 15/10/2016.
+ */
+public class VenueFinderService {
+
+    public List<String> getVenuesNearLocation(String location, int radius) {
+        return null;
+    }
+}
