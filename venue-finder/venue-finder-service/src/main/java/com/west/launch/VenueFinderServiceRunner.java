fee4a1e35250d279cc360150722878fe3b5ffab3
Peter West <peterjameswest@sciencefictionextra.com>
2016-10-15 17:34:14 +0100

    Made into Spring Boot class and imported the new Spring config.

--- venue-finder/venue-finder-service/src/main/java/com/west/launch/VenueFinderServiceRunner.java
+++ venue-finder/venue-finder-service/src/main/java/com/west/launch/VenueFinderServiceRunner.java
@@ -1,7 +1,18 @@
 package com.west.launch;
 
+import com.west.config.SpringConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
 /**
  * Created by westp on 15/10/2016.
  */
+@SpringBootApplication
+@Import(SpringConfig.class)
 public class VenueFinderServiceRunner {
+
+    public static void main(String args[]) {
+        SpringApplication.run(VenueFinderServiceRunner.class, args);
+    }
 }
