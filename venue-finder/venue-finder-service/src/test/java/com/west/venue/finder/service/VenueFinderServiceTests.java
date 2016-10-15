e2f24c3956953e0612b25e51fa3c652620f07b84
Peter West <peterjameswest@sciencefictionextra.com>
2016-10-15 17:35:37 +0100

    Added an initial failing test for the service method.

---
+++ venue-finder/venue-finder-service/src/test/java/com/west/venue/finder/service/VenueFinderServiceTests.java
@@ -0,0 +1,28 @@
+package com.west.venue.finder.service;
+
+import com.west.config.SpringTestConfig;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Created by westp on 15/10/2016.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes= SpringTestConfig.class)
+public class VenueFinderServiceTests {
+
+    @Autowired
+    private VenueFinderService venueFinderService;
+
+    @Test
+    public void testGetVenuesNearLocation() {
+
+        assertNotNull(venueFinderService.getVenuesNearLocation(null, -1));
+    }
+}
