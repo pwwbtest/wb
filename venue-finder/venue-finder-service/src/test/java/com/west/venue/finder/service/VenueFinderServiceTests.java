01f9bfe3ad10737e0cdc1c93eae04e47e75e11a2
Peter West <peterjameswest@sciencefictionextra.com>
2016-10-15 22:05:45 +0100

    Added more specific tests based on responses

--- venue-finder/venue-finder-service/src/test/java/com/west/venue/finder/service/VenueFinderServiceTests.java
+++ venue-finder/venue-finder-service/src/test/java/com/west/venue/finder/service/VenueFinderServiceTests.java
@@ -1,13 +1,14 @@
 package com.west.venue.finder.service;
 
 import com.west.config.SpringTestConfig;
+import com.west.venue.model.Contact;
 import com.west.venue.model.Location;
 import com.west.venue.model.Venue;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
 
 import java.util.List;
@@ -18,7 +19,7 @@ import static org.junit.Assert.assertTrue;
 /**
  * Created by westp on 15/10/2016.
  */
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes= SpringTestConfig.class)
 public class VenueFinderServiceTests {
 
@@ -27,28 +28,87 @@ public class VenueFinderServiceTests {
     @Autowired
     private VenueFinderService venueFinderService;
 
-    @Test
-    public void testGetVenuesNearLocationResponsePopulated() {
 
-        String locationToSearchFor = "Chicago";
+    @Test(expected=VenueFinderServiceException.class)
+    public void testGetVenuesNearLocationNullOauthToken() throws VenueFinderServiceException {
+        String locationToSearchFor = "chicago";
+        int radiusMetres = 800;
+        List<Venue> venues = venueFinderService.getVenuesNearLocation(null, locationToSearchFor, radiusMetres);
+    }
+
+    @Test(expected=VenueFinderServiceException.class)
+    public void testGetVenuesNearLocationEmptyOauthToken() throws VenueFinderServiceException {
+        String locationToSearchFor = "chicago";
+        int radiusMetres = 800;
+        List<Venue> venues = venueFinderService.getVenuesNearLocation("", locationToSearchFor, radiusMetres);
+    }
+
+    @Test(expected=VenueFinderServiceException.class)
+    public void testGetVenuesNearLocationInvalidOauthToken() throws VenueFinderServiceException {
+        String locationToSearchFor = "chicago";
+        int radiusMetres = 800;
+        List<Venue> venues = venueFinderService.getVenuesNearLocation("abc", locationToSearchFor, radiusMetres);
+    }
+
+
+
+    @Test(expected=VenueFinderServiceException.class)
+    public void testGetVenuesNearLocationNullLocation() throws VenueFinderServiceException {
+        String locationToSearchFor = "chicago";
+        int radiusMetres = 800;
+        List<Venue> venues = venueFinderService.getVenuesNearLocation(PUBLIC_OAUTH_TOKEN, null, radiusMetres);
+    }
+
+    @Test(expected=VenueFinderServiceException.class)
+    public void testGetVenuesNearLocationEmptyLocation() throws VenueFinderServiceException {
+        String locationToSearchFor = "chicago";
+        int radiusMetres = 800;
+        List<Venue> venues = venueFinderService.getVenuesNearLocation(PUBLIC_OAUTH_TOKEN, null, radiusMetres);
+    }
+
+    @Test(expected=VenueFinderServiceException.class)
+    public void testGetVenuesNearLocationZeroRadius() throws VenueFinderServiceException {
+        String locationToSearchFor = "chicago";
+        int radiusMetres = 800;
+        List<Venue> venues = venueFinderService.getVenuesNearLocation(PUBLIC_OAUTH_TOKEN, locationToSearchFor, 0);
+    }
+
+    @Test(expected=VenueFinderServiceException.class)
+    public void testGetVenuesNearLocationNegativeRadius() throws VenueFinderServiceException {
+        String locationToSearchFor = "chicago";
+        int radiusMetres = 800;
+        List<Venue> venues = venueFinderService.getVenuesNearLocation(PUBLIC_OAUTH_TOKEN, locationToSearchFor, -1);
+    }
+
+    @Test
+    public void testGetVenuesNearLocationResponsePopulated() throws VenueFinderServiceException {
+
+        String locationToSearchFor = "chicago";
         int radiusMetres = 800;
 
-        List<Venue> venues = venueFinderService.getVenuesNearLocation(PUBLIC_OAUTH_TOKEN, locationToSearchFor, radiusMetres);
-        assertNotNull(venues);
-        assertTrue(venues.size() > 0);
+            List<Venue> venues = venueFinderService.getVenuesNearLocation(PUBLIC_OAUTH_TOKEN, locationToSearchFor, radiusMetres);
+            assertNotNull(venues);
+            assertTrue(venues.size() > 0);
 
-        Location location = null;
-        for(Venue venue: venues) {
-            assertNotNull(venue.getContact());
-            assertNotNull(venue.getId());
-            assertNotNull(venue.getPhone());
-            assertNotNull(venue.getLocation());
+            Location location = null;
+            Contact contact = null;
+            for (Venue venue : venues) {
 
-            location = venue.getLocation();
-            assertNotNull(location.getAddress());
-            assertNotNull(location.getCrossStreet());
-            assertNotNull(location.getLongitude());
-            assertNotNull(location.getLattitude());
+                assertNotNull(venue.getId());
+                assertNotNull(venue.getName());
+                assertNotNull(venue.getLocation());
+
+                location = venue.getLocation();
+                // assertNotNull(location.getAddress()); not always populated
+                //assertNotNull(location.getCrossStreet()); not always populated
+                assertNotNull(location.getLongitude());
+                assertNotNull(location.getLattitude());
+
+                contact = venue.getContact();
+                // assertNotNull(contact.getPhone()); not always populated
+
+                //TODO mock the response so it is always populated for the test
+            }
         }
-    }
+
 }
