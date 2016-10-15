cb567b7c559fbdbc78f587f96ba4c19ab710523d
Peter West <peterjameswest@sciencefictionextra.com>
2016-10-15 18:02:33 +0100

    Test now checks for a list of venues and checks that they all have a location.

--- venue-finder/venue-finder-service/src/test/java/com/west/venue/finder/service/VenueFinderServiceTests.java
+++ venue-finder/venue-finder-service/src/test/java/com/west/venue/finder/service/VenueFinderServiceTests.java
@@ -1,6 +1,7 @@
 package com.west.venue.finder.service;
 
 import com.west.config.SpringTestConfig;
+import com.west.venue.model.Venue;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
+import java.util.List;
+
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by westp on 15/10/2016.
@@ -17,16 +21,23 @@ import static org.junit.Assert.assertNotNull;
 @SpringBootTest(classes= SpringTestConfig.class)
 public class VenueFinderServiceTests {
 
+    private static final String PUBLIC_OAUTH_TOKEN = "T3EEDQMOYVDGVB5LNXNZLKDT5PXUHIW2RHGY4N0QDB5GON0V";
+
     @Autowired
     private VenueFinderService venueFinderService;
 
     @Test
     public void testGetVenuesNearLocation() {
 
-        String oAuthToken = null;
         String location = "Chicago";
         int radiusMetres = 800;
 
-        assertNotNull(venueFinderService.getVenuesNearLocation(oAuthToken, location, radiusMetres));
+        List<Venue> venues = venueFinderService.getVenuesNearLocation(PUBLIC_OAUTH_TOKEN, location, radiusMetres);
+        assertNotNull(venues);
+        assertTrue(venues.size() > 0);
+
+        for(Venue venue: venues) {
+            assertNotNull(venue.getLocation());
+        }
     }
 }
