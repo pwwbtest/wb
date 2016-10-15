6584fb2e7c0b4cd471c0fdd11ee9893f871c30ff
Peter West <peterjameswest@sciencefictionextra.com>
2016-10-15 18:34:04 +0100

    Added missing accessors

--- venue-finder/venue-model/src/main/java/com/west/venue/model/Response.java
+++ venue-finder/venue-model/src/main/java/com/west/venue/model/Response.java
@@ -8,4 +8,12 @@ import java.util.List;
 public class Response {
 
     private List<Venue> venues;
+
+    public List<Venue> getVenues() {
+        return venues;
+    }
+
+    public void setVenues(List<Venue> venues) {
+        this.venues = venues;
+    }
 }
