ab96263ad051ab01a9c943ae9335d6df5f3c69e1
Peter West <peterjameswest@sciencefictionextra.com>
2016-10-15 17:42:58 +0100

    Added  a basic POJO for venue

---
+++ venue-finder/venue-model/src/main/java/com/west/venue/model/Venue.java
@@ -0,0 +1,26 @@
+package com.west.venue.model;
+
+/**
+ * Created by westp on 15/10/2016.
+ */
+public class Venue {
+
+    private String location;
+    private int radius;
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+}
