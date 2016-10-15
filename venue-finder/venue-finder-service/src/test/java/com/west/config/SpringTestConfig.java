02d8fababafaa8dfb68d5afd60e7b4e9df7ce9df
Peter West <peterjameswest@sciencefictionextra.com>
2016-10-15 17:34:57 +0100

    Added Spring config for tests to use, allowing them to override default beans where necessary.

---
+++ venue-finder/venue-finder-service/src/test/java/com/west/config/SpringTestConfig.java
@@ -0,0 +1,11 @@
+package com.west.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by westp on 15/10/2016.
+ */
+@Configuration
+public class SpringTestConfig extends SpringConfig {
+
+}
