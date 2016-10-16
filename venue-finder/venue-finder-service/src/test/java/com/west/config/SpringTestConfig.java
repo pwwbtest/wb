0b0de302c0487b52016bff4a30949eba60611571
Peter West <peterjameswest@sciencefictionextra.com>
2016-10-16 15:49:09 +0100

    Added comment

--- venue-finder/venue-finder-service/src/test/java/com/west/config/SpringTestConfig.java
+++ venue-finder/venue-finder-service/src/test/java/com/west/config/SpringTestConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by westp on 15/10/2016.
+ * Override beans as required for tests.
  */
 @Configuration
 public class SpringTestConfig extends SpringConfig {
