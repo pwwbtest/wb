6c767c3163fbb1736b9f39da69c7253f7083f01c
Peter West <peterjameswest@sciencefictionextra.com>
2016-10-16 14:43:59 +0100

    Refactored to differentiate between a response from FourSquare API and a response from the VenueFinderService

--- venue-finder/venue-model/src/main/java/com/west/venue/model/Venue.java
+++ venue-finder/venue-model/src/main/java/com/west/venue/model/Venue.java
@@ -1,8 +1,11 @@
 package com.west.venue.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 /**
  * Created by westp on 15/10/2016.
  */
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class Venue {
 
     private String id;
